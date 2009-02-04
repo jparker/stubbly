1ec67288dbd04688ca6bace7ea9aa55f4e164f8b
John Parker <jparker@urgetopunt.com>
2009-02-03 20:37:33 -0800

    added capistrano deployment

---
+++ config/deploy.rb
@@ -0,0 +1,21 @@
+set :application, 'stubbly'
+set :repository, 'git@github.com:jparker/stubbly.git'
+
+set :user, 'appserve'
+set :use_sudo, false
+set :deploy_to, '/var/www/stubbly'
+
+set :scm, :git
+set :branch, 'master'
+set :deploy_via, :remote_cache
+
+role :app, 'moa.urgetopunt.com'
+role :web, 'moa.urgetopunt.com'
+role :db, 'moa.urgetopunt.com'
+
+namespace :deploy do
+  desc 'Restart application server'
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
